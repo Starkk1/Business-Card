73c55521d270de0b850b966ab15a777452effd70
Adel <therealstark13@gmail.com>
2024-04-28 18:04:07 +0300

    Fixed Email button to direct to sending an email directly.

--- src/container/Head.jsx
+++ src/container/Head.jsx
@@ -9,7 +9,7 @@ export default function Header() {
             <p className="header--title">Frontend React.js developer</p>
             <p className="header--website"><a href="#" className="header--website" target="_blank">www.abc.com</a></p>
             <div className="btns-group">
-                <a href="therealstark13@gmail.com" className="header--email_btn" target="_blank"><button><i class="fa-solid fa-envelope fa-beat"></i>&nbsp;&nbsp;Email</button></a>
+                <a href="mailto:therealstark13@gmail.com" className="header--email_btn" target="_blank"><button><i class="fa-solid fa-envelope fa-beat"></i>&nbsp;&nbsp;Email</button></a>
                 <a href="https://www.linkedin.com/in/adel-ashraf-3a9445231/" className="header--linkedin_btn" target="_blank"><button><i class="fa-brands fa-linkedin fa-beat"></i>&nbsp;&nbsp;LinkedIn</button></a>
             </div>
         </>
