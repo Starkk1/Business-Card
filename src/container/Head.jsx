1edfa1911dd993b8fb01f20514deb4e84683c050
Adel <therealstark13@gmail.com>
2024-04-28 18:48:48 +0300

    Business card size adjustted on different screen sizes.

--- src/container/Head.jsx
+++ src/container/Head.jsx
@@ -4,13 +4,15 @@ import me from "../assets/me.jpg"
 export default function Header() {
     return (
         <>
-            <img src={me} alt="A picture of myself" className="header--picture" />
-            <h1 className="header--name">Adel Ashraf </h1>
-            <p className="header--title">Frontend React.js developer</p>
-            <p className="header--website"><a href="#" className="header--website" target="_blank">www.abc.com</a></p>
-            <div className="btns-group">
-                <a href="mailto:therealstark13@gmail.com" className="header--email_btn" target="_blank"><button><i class="fa-solid fa-envelope fa-beat"></i>&nbsp;&nbsp;Email</button></a>
-                <a href="https://www.linkedin.com/in/adel-ashraf-3a9445231/" className="header--linkedin_btn" target="_blank"><button><i class="fa-brands fa-linkedin fa-beat"></i>&nbsp;&nbsp;LinkedIn</button></a>
+            <div className="header">
+                <img src={me} alt="A picture of myself" className="header--picture" />
+                <h1 className="header--name">Adel Ashraf </h1>
+                <p className="header--title">Frontend developer</p>
+                <p className="header--website"><a href="https://adel-ashraf-business-card.netlify.app/" className="header--website" target="_blank">This website</a></p>
+                <div className="btns-group">
+                    <a href="mailto:therealstark13@gmail.com" className="header--email_btn" target="_blank"><button><i class="fa-solid fa-envelope fa-beat"></i>&nbsp;&nbsp;Email</button></a>
+                    <a href="https://www.linkedin.com/in/adel-ashraf-3a9445231/" className="header--linkedin_btn" target="_blank"><button><i class="fa-brands fa-linkedin fa-beat"></i>&nbsp;&nbsp;LinkedIn</button></a>
+                </div>
             </div>
         </>
     )
