861bf169336a28ed2045528130c516076e49e261
Adel <therealstark13@gmail.com>
2024-05-05 10:26:59 +0300

    Facebook button removed

--- src/container/Footer.jsx
+++ src/container/Footer.jsx
@@ -5,7 +5,6 @@ export default function Footer(){
         <>
         <div className="footer">
         <a href="https://github.com/Starkk1" target="_blank" className="footer--item"><i class="fa-brands fa-github fa-2x"></i></a>
-        <a href="https://www.facebook.com/profile.php?id=100009122471565" target="_blank" className="footer--item"><i class="fa-brands fa-facebook fa-2x"></i></a>
         <a href="https://www.instagram.com/im_stark1/" target="_blank" className="footer--item"><i class="fa-brands fa-instagram fa-2x"></i></a>
         </div>
         </>
